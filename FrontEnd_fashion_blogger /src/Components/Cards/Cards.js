1447a34fd3cf02042792067321c4028a02776b34
Narendra Malviya <narendramalviya5@gmail.com>
2020-06-24 17:48:30 +0530

    working on modal

--- FrontEnd_fashion_blogger /src/Components/Cards/Cards.js
+++ FrontEnd_fashion_blogger /src/Components/Cards/Cards.js
@@ -1,22 +1,35 @@
 import React, { Component, Fragment } from "react";
 import Card from "./Card";
+import Modal from "../UI/Modal/Modal";
 
 class Cards extends Component {
+    state = {
+        showModal:false,
+    }
+    // method for click action on photo
+    modalHandler = (photoId)=>{
+          this.setState({showModal:true})
+    }
+    // shouldComponentUpdate(){
+    //     return false;
+    // }
+  
 	render() {
-		let cards = [];
-
+        let cards = [];      
 		for (let i = 0; i < 20; i++) {
 			cards.push(i);
-		}
+        }  
 		const allMappedCards = cards.map((val, index) => (
 			<li key={index}>
-				{" "}
+			
 				<Card num={index} />{" "}
 			</li>
-		));
+        ));
+        
 		return (
 			<div className="jumbotron bg-white ">
 				<Fragment>
+                   { this.state.showModal ? <Modal /> : null}
 					<ul
 						className="d-flex flex-row justify-content-start flex-wrap"
 						style={{ listStyle: "none" }}
--- FrontEnd_fashion_blogger /src/Components/Home/Home.js
+++ FrontEnd_fashion_blogger /src/Components/Home/Home.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import Cards from '../Cards/Cards';
+import React, { Component } from "react";
+import Cards from "../Cards/Cards";
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-               <Cards/> 
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<Cards />
+			</div>
+		);
+	}
 }
 
 export default Home;
